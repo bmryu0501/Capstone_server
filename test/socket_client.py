30801eb8fb1abb3d117e6f842dce4184c6e35862
bmryu0501 <bmryu0501@gmail.com>
2022-10-29 23:35:11 +0900

    socket update

--- test/socket_client.py
+++ test/socket_client.py
@@ -1,7 +1,8 @@
+'''
 from socket import *
 
 clientSock = socket(AF_INET, SOCK_STREAM)
-clientSock.connect(('127.0.0.1', 8080))
+clientSock.connect(('13.125.51.21', 8080))
 
 print('연결 확인 됐습니다.')
 clientSock.send('I am a client'.encode('utf-8'))
@@ -10,3 +11,12 @@ print('메시지를 전송했습니다.')
 
 data = clientSock.recv(1024)
 print('받은 데이터 : ', data.decode('utf-8'))
+'''
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect(('13.125.51.213'))
+
+sock.send("hello!".encode())
--- test/socket_server.py
+++ test/socket_server.py
@@ -1,3 +1,4 @@
+'''
 from socket import *
 
 serverSock = socket(AF_INET, SOCK_STREAM)
@@ -13,3 +14,17 @@ print('받은 데이터 : ', data.decode('utf-8'))
 
 connectionSock.send('I am a server.'.encode('utf-8'))
 print('메시지를 보냈습니다.')
+'''
+#-*- coding:utf-8 -*-
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('host', 8080))
+
+server.listen(0)
+
+client, addr = server.accept()
+
+data = client.recv(65535)
+
+print("receieved data:", data.decode())
