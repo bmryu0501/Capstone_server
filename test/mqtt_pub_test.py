363c95db7ac842a8d4ee818a8ee572f50aa9b39e
bmryu0501 <bmryu0501@gmail.com>
2022-10-29 21:35:35 +0900

    MQTT test files added

---
+++ test/mqtt_pub_test.py
@@ -0,0 +1,16 @@
+import paho.mqtt.publish as publish
+
+msgs = \
+[
+    {
+        'topic':"/bm/test",
+        'payload':"multiple 1"
+    },
+
+    (
+        "/bm/test",
+        "multiple 2", 0, False
+    )
+]
+publish.multiple(msgs, hostname="test.mosquitto.org")
+#Topic /seoul/yuokok 에 문자값 multiple 1, multiple 2를 발행한다.
