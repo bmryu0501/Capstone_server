d8edfdcc63819193b89f87ce484820c1e80e1cc7
Ubuntu <ubuntu@ip-172-31-2-206.ap-northeast-2.compute.internal>
2022-10-31 06:38:28 +0000

    update

--- test/socket_server.py
+++ test/socket_server.py
@@ -19,7 +19,7 @@ print('메시지를 보냈습니다.')
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('ubuntu', 8080))
+server.bind(('ip-172-31-2-206.ap-northeast-2.compute.internal', 8080))
 
 server.listen(0)
 
